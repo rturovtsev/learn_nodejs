63e31b9c4a3f56b53adaceecab54d0477b453a3b
Sugrob69 <sugrob.boid@gmail.com>
2016-02-10 22:19:00 +0300

    start

---
+++ start/user/index.js
@@ -0,0 +1,18 @@
+//exports
+//global
+
+var phrases = require('./ru');
+
+function User(name) {
+    this.name = name;
+}
+
+User.prototype.hello = function(who) {
+    console.log(phrases.Hello + ", " + who.name);
+};
+
+console.log("user.js is required");
+
+exports.User = User;
+
+//global.User = User;
