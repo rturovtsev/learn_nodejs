f34f003bb9d1dda57e1c37f9b721824710277ebb
Sugrob69 <sugrob.boid@gmail.com>
2016-02-16 21:50:59 +0300

    jade

--- express/routes/index.js
+++ express/routes/index.js
@@ -4,5 +4,5 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express', body: '<b>Express</b>' });
+  res.render('index', { title: '<b>Express</b>', body: '<b>Express</b>' });
 };
